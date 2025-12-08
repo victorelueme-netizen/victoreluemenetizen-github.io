45cf37ade66d2b5dbfa31b107051f17f9ed829ad
victorelueme-netizen <victor.elueme@svu.edu>
2025-12-08 00:43:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,53 +1,30 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>Calculator</title>
-<style>
-body {
-    font-family: Arial;
-    margin: 50px;
-    background: white;
-    text-align: center;
+let display = document.getElementById('display');
+
+function appendToDisplay(value) {
+    if (display.value === 'Error') {
+        display.value = '';
+    }
+    display.value += value;
 }
 
-h1 {
-    color: black;
+function clearDisplay() {
+    display.value = '';
 }
 
-.calculator {
-    width: 250px;
-    margin: 0 auto;
-    border: 2px solid black;
-    padding: 20px;
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
 }
 
-#display {
-    width: 100%;
-    height: 40px;
-    font-size: 20px;
-    text-align: right;
-    margin-bottom: 10px;
-    border: 1px solid black;
-    background: white;
-    color: black;
+function calculate() {
+    try {
+        let result = eval(display.value);
+        
+        if (result === Infinity || result === -Infinity) {
+            display.value = 'Error';
+        } else {
+            display.value = result;
+        }
+    } catch (error) {
+        display.value = 'Error';
+    }
 }
-
-.buttons {
-    display: grid;
-    grid-template-columns: repeat(4, 1fr);
-    gap: 5px;
-}
-
-button {
-    height: 50px;
-    font-size: 18px;
-    border: 1px solid black;
-    background: white;
-    color: black;
-}
-
-button:hover {
-    background: #f0f0f0;
-}
-</style>
-</
